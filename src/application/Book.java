358b4124fc255e79989d9e5b085588f7316a51c7
hrrrng <124539927+hrrrng@users.noreply.github.com>
2025-07-06 14:51:32 -0700

    implementing Book, Listing, Library
    began implementing basic functionalities of the Book, Listing, and Library classes

---
+++ src/application/Book.java
@@ -0,0 +1,40 @@
+package application;
+
+public class Book {
+	
+	private String category;
+	private float originalPrice;
+	private String title;
+	private String author;
+	private int year;
+	
+	public Book(String category, float originalPrice, String title, String author, int year) {
+	
+		this.category = category;
+		this.originalPrice = originalPrice;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public float getOriginalPrice() {
+		return originalPrice;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+
+}
